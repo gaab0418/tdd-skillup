c9a626e5fd2a557dc3a0178a0bd6471c4b15e848
gaab0418 <zgabrielw.sz13@gmail.com>
2026-06-29 20:36:12 -0300

    fix: warnings

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   timeout: 30000,
+  reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
     headless: true,
